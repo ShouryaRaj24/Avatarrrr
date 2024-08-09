bd6e7e529dfba4463aa2d68bf5cbc5472eed8be0
ShouryaRaj24 <shouryaraj24@gmail.com>
2024-08-09 18:08:50 +0530

    First commit

---
+++ src/main/java/com/demo/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.demo.entity.Course;
+
+public interface CourseRepository extends JpaRepository<Course, Long>{
+
+}
