118af8cf955489017a7c134b40bb5522eb650c45
ShouryaRaj24 <shouryaraj24@gmail.com>
2024-08-12 14:12:49 +0530

    Again Bad commit

--- src/main/java/com/demo/AvatarrrrApplication.java
+++ src/main/java/com/demo/AvatarrrrApplication.java
@@ -8,7 +8,7 @@ public class AvatarrrrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AvatarrrrApplication.class, args);
-		System.out.println("hello");
+		System.out.println("Heyyyyyyyyyy");
 	}
 
 }
