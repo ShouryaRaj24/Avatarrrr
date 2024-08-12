1ecc886ab0ec2a855dfa740e1444a634f8e1145f
ShouryaRaj24 <shouryaraj24@gmail.com>
2024-08-12 13:25:40 +0530

    bad

--- src/main/java/com/demo/AvatarrrrApplication.java
+++ src/main/java/com/demo/AvatarrrrApplication.java
@@ -3,12 +3,16 @@ package com.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 @SpringBootApplication
 public class AvatarrrrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AvatarrrrApplication.class, args);
-		System.out.println("hello");
+		System.out.println("Running");
 	}
+	
+
 
 }
